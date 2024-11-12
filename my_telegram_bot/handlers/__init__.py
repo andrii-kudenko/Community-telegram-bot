c08e14dae488c45a8ee139551ec762252102d4d4
Andrii Kudenko <andrii.kudenko@gmail.com>
2024-11-12 00:08:49 -0500

    Test1

--- my_telegram_bot/handlers/__init__.py
+++
@@ -1,7 +0,0 @@
-from aiogram import Router
-# from friendships_handler import friendships_handler
-
-general_router = Router(name=__name__)
-# general_router.include_routers(
-#     friendship_router,
-# )
